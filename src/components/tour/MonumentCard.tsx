956a62fe3c494edaddba699b8587487175feaf5a
sandeepNIS <sandeep.r@nichi.com>
2026-02-23 11:17:54 +0530

    Card component related change

--- src/components/tour/MonumentCard.tsx
+++ src/components/tour/MonumentCard.tsx
@@ -22,7 +22,7 @@ export default function MonumentCard({
 }: MonumentCardProps) {
     return (
         <motion.div
-            initial={{ opacity: 0, y: 30 }}
+            initial={{ opacity: 0, y: 0 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.8, delay: idx * 0.1, ease: [0.22, 1, 0.36, 1] }}
